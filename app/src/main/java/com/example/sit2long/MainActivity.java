c3970bfdb1e89848efc9a7c263666fa4140f77c0
Patrick <patricki@andrew.cmu.edu>
2021-10-27 19:43:32 +0200

    first push

---
+++ app/src/main/java/com/example/sit2long/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sit2long;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
